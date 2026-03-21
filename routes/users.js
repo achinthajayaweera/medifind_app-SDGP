10c068db78c942d21b86717e05afd70097dbc4f0
nisal20232064 <nisal.20232064@iit.ac.lk>
2026-03-22 00:55:21 +0530

    add user lookup by id endpoint

--- routes/users.js
+++ routes/users.js
@@ -118,4 +118,20 @@ router.get("/", async (req, res) => {
   }
 });
 
+// GET user by id
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findByPk(req.params.id, {
+      attributes: { exclude: ["password"] },
+    });
+
+    if (!user) return res.status(404).json({ error: "User not found" });
+
+    res.json(user);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Something went wrong" });
+  }
+});
+
 module.exports = router;
